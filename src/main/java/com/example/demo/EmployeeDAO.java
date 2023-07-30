c13f6ffb2e4d40d428358454978e9d3a147f53db
naveenb98 <naveenbajjanna@gmail.com>
2023-07-30 22:49:20 +0530

    spring

---
+++ src/main/java/com/example/demo/EmployeeDAO.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeDAO {
+@Autowired
+EmployeeRepo repo;
+
+//insertion 
+public Employee insert(Employee e) {
+	return repo.save(e);
+}
+
+///retrival
+public List<Employee> getall(){
+	return repo.findAll();
+}
+}
